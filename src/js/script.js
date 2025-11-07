28e7ebc12bd480a6853fb5ed0b2526711b6f5f5f
ghazal-sami <sami.ghazali@edu.univ-fcomte.fr>
2025-11-07 16:19:40 +0100

    ajout js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-// TODO
+// Trouver l'élément avec l'ID "a-mettre-en-rouge"
+const aMettreEnRouge = document.querySelector('#a-mettre-en-rouge')
+
+// Changer la couleur du texte de cet élément
+aMettreEnRouge.style.color = 'red'
+
+
+
+// --- Réagir à un click sur le bouton ---
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
+
+enRougeSuiteAClick.addEventListener("click", (evt) => {
+    enRougeSuiteAClick.style.color = "red";
+});
+
+// --- Réagir à un click sur les H2 ---
+const collH2 = document.querySelectorAll("h2");
+
+collH2.forEach((elm) => {
+    elm.addEventListener("click", (evt) => {
+        evt.target.style.color = "red";
+    });
+});
